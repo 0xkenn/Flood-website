3907831d303ceadbca08d0c679d77b2fe59a04ba
0xkenn <kennycharlest@gmail.com>
2023-12-05 01:29:56 +0800

    first commit

---
+++ news.js
@@ -0,0 +1,25 @@
+;(function ($, window, document, undefined) {
+
+  // ########################################
+  // # Define Functions
+  // ########################################
+  
+  function func() {
+    
+  };
+  
+  // ########################################
+  // # Initialize all necessary event handlers
+  // ########################################
+
+  var init = function () {
+    func();
+  };
+  
+  // ########################################
+  // # Call init on Document Ready
+  // ########################################
+
+  $(init);
+  
+  })(window.jQuery, this, this.document);
